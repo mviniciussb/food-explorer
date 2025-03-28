344896803de138f1b1198488b99a0e1d61235b35
Marcos Vinicius <108037526+mviniciussb@users.noreply.github.com>
2025-03-28 20:44:31 -0300

    Criação das configurações gerais do projeto.
    Neste commit eu creiei o theme e o global style do projeto.

--- src/styles/global.js
+++ src/styles/global.js
@@ -3,14 +3,20 @@ import { createGlobalStyle } from "styled-components";
 export default createGlobalStyle`
 
 * {
-    margin: 0;
+        margin: 0;
         padding: 0;
         box-sizing: border-box;
 }
 
 body{
         -webkit-font-smoothing: antialiased;
+        background-color: ${({ theme }) => theme.COLORS.DARK_400};
 
     }
+
+button, a {
+    cursor: pointer;
+    text-decoration: none;
+}
 
 `
